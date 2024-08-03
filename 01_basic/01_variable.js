3e5ae3ae0c71d8b104f424559f47a3d07e076a2c
sandeepsingh1632 <sandeepsinghrathore1433@gmail.com>
2024-08-03 07:51:15 +0000

    variable

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,6 @@
+const accountId =12346
+let accountEmail ="sandeepsingh@gmail.com"
+var accountPassword ="99877898987"
+accountCity ="bikaner"
+console.log(accountEmail);
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,19 @@
+"use strict";//the treat all js coad as newer version
+//alert(3+3)// we are using node js ,not browser
+
+let name="sandeep"
+let age= 24
+let isLoggedIn= true
+
+//number=> 2 to power 53
+//bigint
+//string =""
+//boolean => true/false
+//null => standaloan vlaue
+//undifind =>
+//=>symbol unique
+
+//object=>
+
+    console.log(typeof"sandeeps");
+    
